6128f7ad8f62693f835b2b72bb7224b97fe8135a
Jake Poshepny <jposhepny99@gmail.com>
2025-12-15 14:30:41 -0600

    Activity Photo and Price Update

--- src/app/models/activity.ts
+++ src/app/models/activity.ts
@@ -13,9 +13,45 @@ export const foodActivities: Activity[] = [];
 
 export const entertainmentActivities: Activity[] = [
   {
+    id: 'e4',
+    name: 'Snorkeling',
+    price: 80,
+    image: 'pageMedia/entertainment-snorkeling.jpeg'
+  },
+  {
+    id: 'e5',
+    name: 'Ziplining',
+    price: 100,
+    image: 'pageMedia/entertainment-ziplining.jpeg'
+  },
+  {
+    id: 'e3',
+    name: 'Fishing',
+    price: 150,
+    image: 'pageMedia/entertainment-fishing.jpeg'
+  },
+  {
+    id: 'e8',
+    name: 'Golf',
+    price: 125,
+    image: 'pageMedia/entertainment-golf.jpeg'
+  },
+  {
+    id: 'e6',
+    name: 'Night Club',
+    price: 30,
+    image: 'pageMedia/entertainment-night-club.jpeg'
+  },
+  {
+    id: 'e7',
+    name: 'Art Gallery',
+    price: 15,
+    image: 'pageMedia/entertainment-art-gallery.jpeg'
+  },
+  {
     id: 'e1',
     name: 'Museum',
-    price: 50,
+    price: 25,
     image: 'pageMedia/entertainment-museum.jpeg'
   },
   {
@@ -23,80 +59,44 @@ export const entertainmentActivities: Activity[] = [
     name: 'Bowling',
     price: 20,
     image: 'pageMedia/entertainment-bowling.jpeg'
-  },
-  {
-    id: 'e3',
-    name: 'Fishing',
-    price: 20,
-    image: 'pageMedia/entertainment-bowling.jpeg'
-  },
-  {
-    id: 'e4',
-    name: 'Snorkeling',
-    price: 20,
-    image: 'pageMedia/entertainment-bowling.jpeg'
-  },
-  {
-    id: 'e5',
-    name: 'Ziplining',
-    price: 20,
-    image: 'pageMedia/entertainment-bowling.jpeg'
-  },
-  {
-    id: 'e6',
-    name: 'Night Club',
-    price: 20,
-    image: 'pageMedia/entertainment-bowling.jpeg'
-  },
-  {
-    id: 'e7',
-    name: 'Art Gallery',
-    price: 20,
-    image: 'pageMedia/entertainment-bowling.jpeg'
-  },
-  {
-    id: 'e8',
-    name: 'Golf',
-    price: 20,
-    image: 'pageMedia/entertainment-bowling.jpeg'
-  },
+  }
 ]
 
 export const sightseeingActivities: Activity[] = [
   {
     id: 's1',
     name: 'Volcano Tour',
-    price: 20,
+    price: 250,
     image: 'pageMedia/sightseeing-volcano.jpeg'
   },
   {
     id: 's2',
     name: 'Helicopter Tour',
-    price: 20,
+    price: 300,
     image: 'banners/entertainment.jpeg'
   },
   {
     id: 's3',
     name: 'Boat Tour',
-    price: 20,
-    image: 'pageMedia/entertainment-bowling.jpeg'
+    price: 50,
+    image: 'pageMedia/sightseeing-boat-tour.jpeg'
   },
   {
     id: 's4',
     name: 'Bus Tour',
-    price: 20,
-    image: 'pageMedia/entertainment-bowling.jpeg'
+    price: 50,
+    image: 'pageMedia/sightseeing-bus-tour.jpeg'
   },
   {
     id: 's5',
     name: 'Bike Tour',
-    price: 20,
+    price: 40,
     image: 'pageMedia/transportation-bike.jpeg'
   },
   {
     id: 's6',
     name: 'Rainforest Hike',
-    price: 20,
+    price: 50,
     image: 'pageMedia/sightseeing-rainforest.jpeg'
   },
 ];
