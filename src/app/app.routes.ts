5605cde94d0abbf928d0c0eb3f197550af92a599
Jake Poshepny <jposhepny99@gmail.com>
2025-12-26 00:02:33 -0600

    Icon, Titles, and Meta Tags

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,13 +7,13 @@ import { Orders } from "./components/orders/orders";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: Home },
+  { path: 'home', component: Home, title: 'Taniti | Tropical Paradise' },
   // { path: 'transportation', component: Page, data: { data: transportationData }},
   // { path: 'lodging', component: Page, data: { data: lodgingData }},
   // { path: 'food', component: Page, data: { data: foodData }},
-  { path: 'entertainment', component: Page, data: { data: entertainmentData }},
-  { path: 'sightseeing', component: Page, data: { data: sightseeingData }},
-  { path: 'cart', component: Cart },
-  { path: 'profile', component: Orders },
-  { path: 'faq', component: Page, data: { data: faqData }}
+  { path: 'entertainment', component: Page, data: { data: entertainmentData }, title: 'Taniti | Entertainment' },
+  { path: 'sightseeing', component: Page, data: { data: sightseeingData }, title: 'Taniti | Sightseeing' },
+  { path: 'cart', component: Cart, title: 'Taniti | Cart' },
+  { path: 'profile', component: Orders, title: 'Taniti | Profile' },
+  { path: 'faq', component: Page, data: { data: faqData }, title: 'Taniti | FAQ' }
 ];
